af63b18de7014896b634edde2d315a280e87c034
Mike Lindegarde <mlindegarde@shinc.org>
2019-04-08 22:22:35 -0400

    Blah.

--- Program.fs
+++ Program.fs
@@ -2,78 +2,56 @@
 open Microsoft.ML
 open Microsoft.ML.Data
 
-type SentimentData () =
-    [<DefaultValue>]
-    [<LoadColumn(0)>]
-    val mutable public SentimentText :string
+open Demo.SentimentAnalysis.Models
+open Microsoft.ML.Data
+open Microsoft.ML.Trainers.FastTree
 
-    [<DefaultValue>]
-    [<LoadColumn(1)>]
-    val mutable public Label :bool
+let displayEvaluation (ml : MLContext) (model : TransformerChain<BinaryPredictionTransformer<FastForestClassificationModelParameters>>) description data = 
+    let predictions = model.Transform data
+
+    let metrics = ml.BinaryClassification.Evaluate(predictions)
+
+    printfn ""
+    printfn "### %s" description
+    printfn "Accuracy          : %0.4f" (metrics.Accuracy)
+    printfn "F1                : %0.4f" (metrics.F1Score)
+    printfn "Positive Precision: %0.4f" (metrics.PositivePrecision)
+    printfn "Positive Recall   : %0.4f" (metrics.PositiveRecall)
+    printfn "Negative Precision: %0.4f" (metrics.NegativePrecision)
+    printfn "Negative Recall   : %0.4f" (metrics.NegativeRecall)
+    printfn ""
+
+    let preview = predictions.Preview()
     
-    // NOTE: Need to add this column to extract metrics
-    [<DefaultValue>]
-    [<LoadColumn(2)>]
-    val mutable public Probability :float32
-   
-type SentimentPrediction () =
-    [<DefaultValue>]
-    val mutable public SentimentData :string
-
-    [<DefaultValue>]
-    val mutable public PredictedLabel :bool
-
-    [<DefaultValue>]
-    val mutable public Score :float32 
+    preview.RowView
+    |> Seq.take 5
+    |> Seq.iter(fun row ->
+        row.Values
+        |> Array.iter (fun kv -> printfn "%s: %A" kv.Key kv.Value)
+        printfn "")
+    printfn ""
 
 [<EntryPoint>]
 let main argv =
-    let ml = MLContext()
+    let context = MLContext()
 
-    let loader = ml.Data.CreateTextLoader<SentimentData>(separatorChar = '\t', hasHeader = false)
+    let loader = context.Data.CreateTextLoader<SentimentData>(separatorChar = '\t', hasHeader = false)
            
     let dataFile = "./data/imdb_labelled.txt"
     let allData = loader.Load(dataFile)
-    let splitData = ml.Clustering.TrainTestSplit(allData, testFraction=0.3)
+    let splitData = context.Clustering.TrainTestSplit(allData, testFraction=0.3)
 
     let pipeline = 
-        ml
+        context
             .Transforms.Text.FeaturizeText("Features", "SentimentText")
-            .Append(ml.BinaryClassification.Trainers.FastForest())
-            // Example of custom hyperparameters
-            // .Append(mlContext.BinaryClassification.Trainers.FastForest(numTrees = 500, numLeaves = 100, learningRate = 0.0001))
+            .Append(context.BinaryClassification.Trainers.FastForest())
 
     let model = pipeline.Fit(splitData.TrainSet)
 
-    let displayEvaluation description data = 
-        let predictions = model.Transform data
+    displayEvaluation context model "Train" splitData.TrainSet
+    displayEvaluation context model "Test" splitData.TestSet
 
-        let metrics = ml.BinaryClassification.Evaluate(predictions)
-
-        printfn ""
-        printfn "### %s" description
-        printfn "Accuracy          : %0.4f" (metrics.Accuracy)
-        printfn "F1                : %0.4f" (metrics.F1Score)
-        printfn "Positive Precision: %0.4f" (metrics.PositivePrecision)
-        printfn "Positive Recall   : %0.4f" (metrics.PositiveRecall)
-        printfn "Negative Precision: %0.4f" (metrics.NegativePrecision)
-        printfn "Negative Recall   : %0.4f" (metrics.NegativeRecall)
-        printfn ""
-
-        let preview = predictions.Preview()
-        
-        preview.RowView
-        |> Seq.take 5
-        |> Seq.iter(fun row ->
-            row.Values
-            |> Array.iter (fun kv -> printfn "%s: %A" kv.Key kv.Value)
-            printfn "")
-        printfn ""
-
-    displayEvaluation "Train" splitData.TrainSet
-    displayEvaluation "Test" splitData.TestSet
-
-    let predictor = model.CreatePredictionEngine<SentimentData, SentimentPrediction>(ml)
+    let predictor = model.CreatePredictionEngine<SentimentData, SentimentPrediction>(context)
 
     let tests = 
         [
@@ -95,11 +73,11 @@ let main argv =
         )
 
     // Save model to file
-    let saveModel (ml:MLContext) trainedMode = 
+    let saveModel (ml:MLContext) trainedModel = 
         use fsWrite = new FileStream("test-model.zip", FileMode.Create, FileAccess.Write, FileShare.Write)
-        ml.Model.Save(model, fsWrite)
+        ml.Model.Save(trainedModel, fsWrite)
 
-    saveModel ml model
+    saveModel context model
 
     // Load model from file
     use fsRead = new FileStream("test-model.zip", FileMode.Open, FileAccess.Read, FileShare.Read)
